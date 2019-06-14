47fa81704810578e5659e19018abbd1a1990cac5
liumingxin@gennlife.com <18645484821@163.com>
2019-06-14 16:10:23 +0800

    增加线程名字

--- src/main/java/com/gennlife/rws/util/SingleExecutorService.java
+++ src/main/java/com/gennlife/rws/util/SingleExecutorService.java
@@ -2,16 +2,18 @@ package com.gennlife.rws.util;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SingleExecutorService {
 
     private volatile static SingleExecutorService instance;
 
-    private final ExecutorService searchUqlExecutorService = Executors.newFixedThreadPool(100);
-    private final ExecutorService cortrastiveAnalysisExecutor = Executors.newFixedThreadPool(8);
-    private final ExecutorService referenceActiveExecutor = Executors.newFixedThreadPool(100);
-    private final ExecutorService flushCountGroupExecutor = Executors.newFixedThreadPool(20);
-    private final ExecutorService centerTaskeExecutor = Executors.newFixedThreadPool(10);
+    private final ExecutorService searchUqlExecutorService = Executors.newFixedThreadPool(100,new TestThreadFactory("searchUqlExecutorService"));
+    private final ExecutorService cortrastiveAnalysisExecutor = Executors.newFixedThreadPool(8,new TestThreadFactory("cortrastiveAnalysisExecutor"));
+    private final ExecutorService referenceActiveExecutor = Executors.newFixedThreadPool(100,new TestThreadFactory("referenceActiveExecutor"));
+    private final ExecutorService flushCountGroupExecutor = Executors.newFixedThreadPool(20,new TestThreadFactory("flushCountGroupExecutor"));
+    private final ExecutorService centerTaskeExecutor = Executors.newFixedThreadPool(10,new TestThreadFactory("centerTaskeExecutor"));
 
     private SingleExecutorService(){}
 
@@ -44,5 +46,32 @@ public class SingleExecutorService {
 
     public ExecutorService getCenterTaskeExecutor() {
         return centerTaskeExecutor;
+    }
+
+    static class TestThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        TestThreadFactory(String name) {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() :
+                Thread.currentThread().getThreadGroup();
+            namePrefix = name + "-" +
+                poolNumber.getAndIncrement() +
+                "-thread-";
+        }
+
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                namePrefix + threadNumber.getAndIncrement(),
+                0);
+            if (t.isDaemon())
+                t.setDaemon(false);
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+            return t;
+        }
     }
 }
